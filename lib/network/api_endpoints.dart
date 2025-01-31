fe6c5a46fe2b69847ad0bd07df47466e70c8bcd4
saurabhwagh124 <saurabhwagh124@gmail.com>
2025-01-31 11:27:27 +0530

    temp push

--- lib/network/api_endpoints.dart
+++ lib/network/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static String baseUrl = "http://192.168.0.105:8888";
+  static String baseUrl = "http://192.168.89.22:8888";
   static String loginUrl = "$baseUrl/login";
   static String registerUrl = "$baseUrl/register";
   static String getAllDoctorsUrl = "$baseUrl/doctors";
